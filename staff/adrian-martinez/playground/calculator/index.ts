f3eeadc8eaa18281a5234da26f1d76e76b5ef42b
Strider <adrian.martinez89@gmail.com>
2024-07-09 20:04:48 +0200

    separating logic and test file #9

--- staff/adrian-martinez/playground/calculator/index.ts
+++ staff/adrian-martinez/playground/calculator/index.ts
@@ -14,14 +14,6 @@ function calculate(op: "add" | "sub" | "mul" | "div", a: number, b: number){
 
     else if(op === "div") return a / b;
 
-    else return "Error";
 }
 
-console.log(calculate("add", 1, 2))
-//3
-console.log(calculate("sub", 1, 2))
-//-1
-console.log(calculate("mul", 1, 2))
-//2
-console.log(calculate("div", 1, 2))
-//0.5
+export default calculate;
